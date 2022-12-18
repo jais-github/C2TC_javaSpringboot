9be894a81d92762b558445f9a8cc82640d303e77
Ayush <jaisayush472@gmail.com>
2022-12-19 00:46:27 +0530

    first commit

---
+++ A6Placement/src/main/java/com/cg/pm/A6PlacementApplication.java
@@ -0,0 +1,13 @@
+package com.cg.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A6PlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A6PlacementApplication.class, args);
+	}
+
+}
